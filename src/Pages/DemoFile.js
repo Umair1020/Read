e005590e54d44deb738e7e3165c8ce3b7f39f46a
umairmuatafa254@gmail.com <umairmustafa659@gmail.com>
2023-12-12 15:22:30 +0500

    ok

--- src/Pages/DemoFile.js
+++ src/Pages/DemoFile.js
@@ -39,7 +39,7 @@ function DemoFile({ pdfDoc, userId }) {
   }
   const [showLogoAndText, setShowLogoAndText] = useState(true);
 
-
+  
   const [isLoading, setIsLoading] = useState(false);
   let confirm;
   // State to store chat messages
@@ -154,15 +154,15 @@ function DemoFile({ pdfDoc, userId }) {
                   />
                 ))}
                 {showLogoAndText && (
-                  <div style={{ position: "absolute", top: "25%", left: "35%" }}>
-                    <img
-                      className="d-flex justify-content-center align-items-center mx-auto"
-                      alt="Logo"
-                      src={logo}
-                    />
-                    <h2 className="text-start">How can I help you today?</h2>
-                  </div>
-                )}
+          <div style={{ position: "absolute", top: "25%", left: "35%" }}>
+            <img
+              className="d-flex justify-content-center align-items-center mx-auto"
+              alt="Logo"
+              src={logo}
+            />
+            <h2 className="text-start">How can I help you today?</h2>
+          </div>
+        )}
                 {showOptions && (
                   <div
                     style={{
@@ -311,11 +311,11 @@ function DemoFile({ pdfDoc, userId }) {
 
                 {showOptions && (
                   <div
-                    style={{
-                      margin: "auto",
-                      borderColor: "rgba(0,0,0,.1)",
-                      overflowY: "scroll",
-                    }}
+                  style={{
+                    margin: "auto",
+                    borderColor: "rgba(0,0,0,.1)",
+                    overflowY: "scroll",
+                  }}
                   >
                     {/* Option buttons with specific content */}
                     <div>
@@ -336,7 +336,7 @@ function DemoFile({ pdfDoc, userId }) {
                         <div className="flex flex-col overflow-hidden">
                           <div className=" font-normal">Find Summary</div>
                           <p className=" opacity-50 w-100">
-
+                           
                             Get a quick summary of the PDF content
                           </p>
                         </div>
